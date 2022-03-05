1fc012a6f6a7ff5dc9fb8be3d27d25048ba67826
kotahorii <khorii.micron@gmail.com>
2022-03-05 16:00:55 +0900

    [update] file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,29 @@
 fun main() {
-    val oddNumbers = mutableListOf<Int>().run {
-        for (i in 1..10) {
-            if (i % 2 == 1) add(i)
-        }
-        joinToString(separator = " ")
-    }
-    println(oddNumbers)
+    val executorDelegate = AddCalculationExecutorDelegate(CommonCalculationExecutor("Takashi"))
+    executorDelegate.printStartMessage()
+    println(executorDelegate.calc(8, 11))
 }
 
+
+interface CalculationExecutor {
+    val message: String
+    fun calc(num1: Int, num2: Int): Int
+    fun printStartMessage()
+}
+
+class CommonCalculationExecutor(override val message: String = "calc") : CalculationExecutor {
+    override fun calc(num1: Int, num2: Int): Int {
+        throw IllegalStateException("Not implements calc")
+    }
+
+    override fun printStartMessage() {
+        println("start $message")
+    }
+}
+
+class AddCalculationExecutorDelegate(private val calculationExecutor: CalculationExecutor) :
+    CalculationExecutor by calculationExecutor {
+    override fun calc(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
